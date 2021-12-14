28ac2934d701b7def7cb9cce747eb6fb5e5681d9
DmytroPundyk <dimonpund@gmail.com>
2021-12-14 15:53:13 +0200

    fix

--- src/components/social-profile/Profile.js
+++ src/components/social-profile/Profile.js
@@ -5,7 +5,7 @@ import styles from './profile.module.css';
 const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className={styles.profile}>
     <div className={styles.description}>
-      <img src={avatar} alt="user avatar" className={styles.avatar} />
+      <img src={avatar} alt={username} className={styles.avatar} />
       <p className={styles.username}>{username}</p>
       <p className={styles.tag}>@{tag}</p>
       <p className={styles.location}>{location}</p>
