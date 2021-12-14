1a1f88f15a1073cd99b4c50337496ecaffec268d
DmytroPundyk <dimonpund@gmail.com>
2021-12-14 14:54:50 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import FriendList from './components/FriendsList/FriendList';
 import friends from './components/FriendsList/friends.json';
 import Profile from './components/social-profile/Profile';
 import user from './components/social-profile/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticsData from './components/Statistics/data.json';
+import TransactionHistory from './components/transaction-history/TransactionHistory';
+import transactions from './components/transaction-history/transactions.json';
 
 
 const App = () => (
   <>
-    {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
-    <FriendList friends={friends} />
-     <Profile
+    <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
     />
-    {/* <TransactionHistory items={transactions} /> */}
+    <Statistics title="Upload stats" stats={statisticsData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
   </>
 );
 
